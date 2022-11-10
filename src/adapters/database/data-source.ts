362a0c51141a48047a9f163b6072787869526f0d
Leonardo Antunes <leonardoantunes1401@gmail.com>
2022-11-10 16:43:22 -0300

    deploy

--- src/adapters/database/data-source.ts
+++ src/adapters/database/data-source.ts
@@ -1,14 +1,18 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import PatientModel from "./models/PatientModel"
+import { readFileSync } from "fs"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
-    host: "localhost",
+    host: "symbian-database-test-2022.mysql.database.azure.com",
     port: 3306,
-    username: "root",
-    password: "12345678",
+    username: "symbian_adm",
+    password: "S1m3d@2022!",
     database: "symbian_med_db",
+    ssl: {
+        ca : readFileSync('src/adapters/database/security/CA.crt.pem').toString()
+    },
     synchronize: true,
     logging: ["error", "info"],
     entities: [PatientModel],
